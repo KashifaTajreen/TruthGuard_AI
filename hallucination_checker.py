d759d99f3fffbd4bf33b3c4ea770b43021e9d20d
KashifaTajreen <kashtaj777@gmail.com>
2026-03-12 20:23:15 +0530

    Clean up commented code in hallucination_checker.py
    Removed commented-out code for the check_wikipedia function and its related logic.

--- hallucination_checker.py
+++ hallucination_checker.py
@@ -2,21 +2,6 @@
 
 import requests
 
-# def check_wikipedia(query):
-
-#     url = "https://en.wikipedia.org/api/rest_v1/page/summary/" + query
-
-#     try:
-#         response = requests.get(url)
-
-#         if response.status_code == 200:
-#             data = response.json()
-#             return data.get("extract", "")
-
-#     except:
-#         return None
-
-import requests
 
 def check_wikipedia(query):
 
@@ -50,4 +35,55 @@ def hallucination_score(ai_answer, wiki_text):
     score = len(overlap) / max(len(ai_words), 1)
 
     return round(score * 100)
+
+# import requests
+
+# # def check_wikipedia(query):
+
+# #     url = "https://en.wikipedia.org/api/rest_v1/page/summary/" + query
+
+# #     try:
+# #         response = requests.get(url)
+
+# #         if response.status_code == 200:
+# #             data = response.json()
+# #             return data.get("extract", "")
+
+# #     except:
+# #         return None
+
+# import requests
+
+# def check_wikipedia(query):
+
+#     url = "https://en.wikipedia.org/api/rest_v1/page/summary/" + query
+
+#     try:
+#         r = requests.get(url)
+
+#         if r.status_code == 200:
+#             data = r.json()
+
+#             return {
+#                 "text": data.get("extract"),
+#                 "source": data.get("content_urls",{}).get("desktop",{}).get("page")
+#             }
+
+#     except:
+#         return None
+
+
+# def hallucination_score(ai_answer, wiki_text):
+
+#     if not wiki_text:
+#         return 50
+
+#     ai_words = set(ai_answer.lower().split())
+#     wiki_words = set(wiki_text.lower().split())
+
+#     overlap = ai_words.intersection(wiki_words)
+
+#     score = len(overlap) / max(len(ai_words), 1)
+
+    # return round(score * 100)
     
