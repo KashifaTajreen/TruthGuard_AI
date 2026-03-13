9a67f52bee99b61fa18e5f379babea9c0228eb9f
KashifaTajreen <kashtaj777@gmail.com>
2026-03-13 13:53:37 +0530

    Update llm_handler.py

--- llm_handler.py
+++ llm_handler.py
@@ -20,24 +20,4 @@ def get_ai_response(prompt):
         )
         return completion.choices[0].message.content
     except Exception as e:
-        # Returns the specific error to help you debug during the demo
         return f"ERROR: SYSTEM FAILURE - {str(e)[:50]}"
-# import streamlit as st
-# from groq import Groq
-
-# def get_ai_response(prompt):
-#     try:
-#         api_key = st.secrets["TAVILY_API_KEY"]
-#         client = Groq(api_key=api_key)
-        
-#         completion = client.chat.completions.create(
-#             model="llama-3.1-8b-instant", # The most stable Groq model right now
-#             messages=[
-#                 {"role": "system", "content": "You are a top-secret intelligence AI. Give short, factual answers."},
-#                 {"role": "user", "content": prompt}
-#             ],
-#             temperature=0.1
-#         )
-#         return completion.choices[0].message.content
-#     except Exception as e:
-#         return f"ERROR: SYSTEM FAILURE - {str(e)}"
